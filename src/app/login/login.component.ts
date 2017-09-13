5c7d858700effdf568c98352714b8188db46185d
Sopyan M <sopyan.mulyana@mitrais.com>
2017-09-13 14:21:58 +0800

    Update service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 // import { AlertService } from '../alert.service';
-import { AuthenticationService } from '../authentication.service';
+import { AuthenticationService } from '../services/authentication.service';
 import { MdSnackBar } from '@angular/material';
 
 @Component({
