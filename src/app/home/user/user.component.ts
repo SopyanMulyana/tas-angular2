1b23068dc0716bbec38a62d383205e985431a2d9
Sopyan M <sopyan.mulyana@mitrais.com>
2017-09-20 18:00:12 +0800

    edit some view on user

--- src/app/home/user/user.component.ts
+++ src/app/home/user/user.component.ts
@@ -14,7 +14,7 @@ import { ListUserService } from "../../services/list-user.service";
 import { ListUser } from "./list-user";
 import { AddUserDialog } from "./add-user-dialog.component";
 
-
+declare var $:any;
 
 //Structure
 @Component({
@@ -25,90 +25,76 @@ import { AddUserDialog } from "./add-user-dialog.component";
 export class UserComponent{
   userRole;
   users;
-  displayedColumns = ['employeeId', 'fullName', 'email', 'jobFamily', 'grade', 'accountName', 'active', 'role', 'action'];
-  userDatabase;
-  dataSource: UserDataSource | null;
-  // private listUserService: ListUserService;
+  // displayedColumns = ['employeeId', 'fullName', 'email', 'jobFamily', 'grade', 'accountName', 'active', 'role', 'action'];
+  // userDatabase;
+  // dataSource: UserDataSource | null;
+  // // private listUserService: ListUserService;
 
-  @ViewChild(MdPaginator) _paginator: MdPaginator;
-  @ViewChild('filter') filter: ElementRef;
-  constructor(public addUser: MdDialog, public listUserService: ListUserService) {
-       this.listUserService.getUsers().subscribe((users => {
-         this.users = users;
-         this.userDatabase = new UserDatabase(this.users); 
-         this.dataSource = new UserDataSource(this.userDatabase, this._paginator);
-       }));
-     }
+  // @ViewChild(MdPaginator) _paginator: MdPaginator;
+  // @ViewChild('filter') filter: ElementRef;
+  constructor(public addUser: MdDialog, public listUserService: ListUserService) { }
   
 
-  ngOnInit() {
+  ngOnInit(listUserService: ListUserService) {
     //below is for filter
-    Observable.fromEvent(this.filter.nativeElement, 'keyup')
-        .debounceTime(150)
-        .distinctUntilChanged()
-        .subscribe(() => {
-          if (!this.dataSource) { return; }
-          this.dataSource.filter = this.filter.nativeElement.value;
-        });
+    // Observable.fromEvent(this.filter.nativeElement, 'keyup')
+    //     .debounceTime(150)
+    //     .distinctUntilChanged()
+    //     .subscribe(() => {
+    //       if (!this.dataSource) { return; }
+    //       this.dataSource.filter = this.filter.nativeElement.value;
+    //     });
         var user = JSON.parse(localStorage.getItem('currentUser'));
         this.userRole = user.role;
+        var dataSet = [];
+        this.listUserService.getUsers().subscribe((users => {
+          $.each(users, (i, item) => {
+            dataSet[i] = $.map(item, function(el) { return el; })
+          });
+          $('#example').on('click', 'a.editor_edit', function (e) {
+            e.preventDefault();
+        } );
+     
+        // Delete a record
+        $('#example').on('click', 'a.editor_remove', function (e) {
+            e.preventDefault();
+        } );
+
+        $('#example').DataTable( {
+          // columnDefs: [
+          //     {
+          //         targets: [ 0, 1, 2 ],
+          //         className: 'mdl-data-table__cell--non-numeric'
+          //     }
+          // ],
+          data: dataSet,
+          columns: [
+              { title: "Employee Id" },
+              { title: "Full Name" },
+              { title: "Email" },
+              { title: "Job Family" },
+              { title: "Grade" },
+              { title: "Account Name" },
+              { title: "Active" },
+              { title: "Role" },
+              {
+                title: "action",
+                defaultContent: '<a href="" class="editor_edit">Edit</a> / <a href="" class="editor_remove">Delete</a>'
+              }
+          ]
+      } );
+    }));
   }
   
 
-  openDialogAdd(): void {
-    let dialogRef = this.addUser.open(AddUserDialog, {
-      width: '80%',
-      data: {  }
-    });
+  // openDialogAdd(): void {
+  //   let dialogRef = this.addUser.open(AddUserDialog, {
+  //     width: '80%',
+  //     data: {  }
+  //   });
 
-    dialogRef.afterClosed().subscribe(result => {
-    });
-  }
+  //   dialogRef.afterClosed().subscribe(result => {
+  //   });
+  // }
 
-}
-
-export class UserDatabase{
-  
-  dataChange: BehaviorSubject<ListUser[]> = new BehaviorSubject<ListUser[]>([]);
-  get data(): ListUser[] { return this.dataChange.value; }
-  
-  constructor(private users: ListUser[]) {
-      this.dataChange.next(users);
-  } 
-}
-
-export class UserDataSource extends DataSource<any> {
-  _filterChange = new BehaviorSubject('');
-  get filter(): string { return this._filterChange.value; }
-  set filter(filter: string) {
-    this._filterChange.next(filter);
-  }
-  filteredData: ListUser[] = [];
-  renderedData: ListUser[] = [];
-  constructor(private _userDatabase: UserDatabase, private _paginator: MdPaginator) {
-    super();
-  }
-  connect(): Observable<ListUser[]> {
-    const displayDataChanges = [
-      this._userDatabase.dataChange,
-      this._filterChange,
-      this._paginator.page,
-    ];
-
-
-    return Observable.merge(...displayDataChanges).map(() => {
-      this.filteredData = this._userDatabase.data.slice().filter((item: ListUser) => {
-        let searchStr = (item.fullName).toLowerCase();
-        return searchStr.indexOf(this.filter.toLowerCase()) != -1;
-      });
-
-
-      // Grab the page's slice of data.
-      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
-      this.renderedData = this.filteredData.splice(startIndex, this._paginator.pageSize);
-      return this.renderedData;
-    });
-  }
-
-  disconnect() {}
 }
